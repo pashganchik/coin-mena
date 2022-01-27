aa0d212352325b5283713b427a5a3ef07752c550
Pavel Manikhov <pavel.manikhov@xyleme.com>
2022-01-27 20:00:34 +0300

    refresh token

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ export const Const = Object.freeze({
     //ApiBaseUrl: 'https://github.com/trending', // NOT returns json data, returns raw HTML
     ApiBaseUrl: 'https://api.github.com',
     FakeCurrentUserId: 20921259,
-    GitHubAccessToken: 'ghp#_U6JmSqRebgej5B4iOqYWqueQe7wwlg0JgraO', // to have an ability to make multiple api calls
+    GitHubAccessToken: 'gh###p_5gKD87y###iQGeVNXLjooK03wnO5TeKRf0drbqK', // to have an ability to make multiple api calls
     SpokenLanguages: ['En', 'De', 'Zh'],
     Languages: ['Java', 'JavaScript', 'TypeScript', 'CSS', 'Python', 'Go', 'Ruby', 'Erlang', 'Emacs Lisp', 'Php'],
     DateRanges: ['daily', 'weekly', 'monthly'],
